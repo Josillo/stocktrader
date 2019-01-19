e57776d9ef75ef80664867c9ec0c069d35531dae
Jose An <jose.jaramago101@gmail.com>
2019-01-19 23:13:43 +0100

    fixes header bootstrap

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import VueRouter from "vue-router";
-import BootstrapVue from "bootstrap-vue";
 
 import App from "./App";
 import { routes } from "./routes";
 
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(BootstrapVue);
 
 const router = new VueRouter({
   mode: "history",
