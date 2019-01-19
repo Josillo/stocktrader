8d61693839a46d9a1cc24d5cdecabbbd5300615e
Jose An <jose.jaramago101@gmail.com>
2019-01-19 23:09:52 +0100

    header created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-md-12"><router-view></router-view></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
+import Header from "./components/Header.vue";
 export default {
-  name: "App",
   components: {
-    HelloWorld
+    appHeader: Header
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import App from "./App";
+import VueRouter from "vue-router";
+import BootstrapVue from "bootstrap-vue";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import { routes } from "./routes";
+
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
-  components: { App },
-  template: "<App/>"
+  router,
+  render: h => h(App)
 });
